ac5af249b45618d352232021c96c6bf5e4c7ac08
Nikhil <nikhil.hindocha@radically.digital>
2020-06-23 14:54:41 +0100

    completed the desktop and mobile header styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,9 @@
 import '/Users/nikhilhindocha/Desktop/project/travel-site/app/assets/styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
 }
+
