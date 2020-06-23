0b174a5eb15f4d2db3184c590c7245b9b1879a65
Nikhil <nikhil.hindocha@radically.digital>
2020-06-23 18:23:50 +0100

    completed the reveal on scroll behaviour

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,11 @@
 import '/Users/nikhilhindocha/Desktop/project/travel-site/app/assets/styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
-
